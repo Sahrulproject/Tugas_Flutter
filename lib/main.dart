e8f3a2bdaac91e672d8439389286631b5e955748
Sahrulproject <sahrulhakim15@gmail.com>
2025-08-04 10:05:10 +0700

    Tugas FlutterD6

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ppkd_b_3/day6/flutter2_1.dart';
 import 'package:ppkd_b_3/start.dart';
 
 void main() {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: StartWidget(),
+      home: Flutter2(),
     );
   }
 }
